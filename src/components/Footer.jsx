a23a7731c25eae3f02e5f931a886c200f3bfbcb1
princejacob528 <princejacob528@gmail.com>
2024-09-13 23:12:27 +0530

    todo list footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,9 @@
-// src/components/Footer.jsx
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="text-white pb-0">
-      <div className="container mx-auto text-center">
+    <div className="fixed bottom-0 left-0 w-full  bg-zinc-900 bg-opacity-50 backdrop-filter backdrop-blur-xl text-blue-400 shadow-xl py-4">
+      <div className="container mx-auto text-center px-4">
         <p className="mt-1">
           More information, mail us at{' '}
           <a href="mailto:princejacob528@gmail.com" className="text-teal-400">
@@ -18,7 +17,7 @@ const Footer = () => {
           <a href="https://www.linkedin.com/in/prince-jacob-bb979b274?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B91wleIXnQcOtAodd9mNyqw%3D%3D" className="text-teal-400" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="p-6 pb-0">
-      <div className="container">
-        <a href="/" className="flex items-center">
-          <img src="/Logo_NBG_Wide.png" alt="PrinceDev Logo" className="w-28" />
-        </a>
-      </div>
-    </header>
+    <div className="fixed top-0 left-0 m-4 ml-6">
+      <a href="/">
+        <img
+          src="/Logo_NBG_Wide.png"
+          alt="Brand Logo"
+          className="h-20 w-auto"
+        /></a>
+    </div>
   );
 };
 
